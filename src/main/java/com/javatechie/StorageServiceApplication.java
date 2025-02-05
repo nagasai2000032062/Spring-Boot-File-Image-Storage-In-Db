63ddb66a0d08ca9934e8cd1046385ab373bb387e
nagasai2000032062 <2000032062@kluniversity.in>
2025-02-05 14:12:06 +0530

    modified

--- src/main/java/com/javatechie/StorageServiceApplication.java
+++ src/main/java/com/javatechie/StorageServiceApplication.java
@@ -36,7 +36,6 @@ public class StorageServiceApplication {
 
 	}
 
-
 	@PostMapping("/fileSystem")
 	public ResponseEntity<?> uploadImageToFIleSystem(@RequestParam("image")MultipartFile file) throws IOException {
 		String uploadImage = service.uploadImageToFileSystem(file);
